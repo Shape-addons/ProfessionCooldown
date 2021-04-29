3683c02c138094f9ce6cafa4a5c8b9fe92527aeb
niels <nille-ej@hotmail.com>
2021-04-29 21:53:06 +0200

    add build script and new zip

---
+++ build.ps1
@@ -0,0 +1,7 @@
+mkdir ProfessionCooldown
+cp .\LICENSE .\ProfessionCooldown\
+cp .\pcd.lua .\ProfessionCooldown\
+cp .\README.md .\ProfessionCooldown\
+cp .\ProfessionCooldown.toc .\ProfessionCooldown\
+7z.exe a ProfessionCooldown.zip .\ProfessionCooldown
+rm -r .\ProfessionCooldown
