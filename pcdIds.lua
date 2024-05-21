cd103ff3962674b7a07e5616cec8fc6d5d1951e8
nieejl <nille-ej@hotmail.com>
2024-05-22 00:22:17 +0200

    add item ids for items, since spell icons are unreliable

--- pcdIds.lua
+++ pcdIds.lua
@@ -1,5 +1,6 @@
 -- local allVanillaTransmuteIds = {
 allTransmuteIds = {
+    -- vanilla transmutes with 1 day cd
     17560, -- Fire to Earth
     11479, -- Iron to Gold
     11480, -- Mithril to Truesilver
@@ -10,7 +11,7 @@ allTransmuteIds = {
     17564, -- Water to Undeath
     17565, -- Life to Earth
     17566, -- Earth to Life
-    17187, -- Arcanite
+    17187, -- Arcanite (no cd after classic)
     25146, -- Elemental Fire (low cd)
 -- }
 -- local allTbcTransmuteIds = {
@@ -24,12 +25,12 @@ allTransmuteIds = {
     28584, -- Primal Life to Earth
     28585, -- Primal Earth to Life
     28580, -- Primal Shadow to Water
-    29688, -- Primal Might
+    29688, -- Primal Might (no cd after tbc)
     32765, -- Earthstorm Diamond
     32766, -- Skyfire Diamond
     -- }
-    -- local allWrathTransmuteIds = {
-    60350, -- Titanium
+    -- wotlk
+    60350, -- Titanium (no cd)
     53784, -- Eternal Water to Fire
     53783, -- Eternal Water to Air
     53782, -- Eternal Earth to Shadow
@@ -48,43 +49,79 @@ allTransmuteIds = {
     66662, -- Dreadstone
     66660, -- King's Amber
     66658, -- Ametrine
-    -- Below has no cd in WOTLK
-    57427, -- Earthsiege Diamond
-    57425, -- Skyflare Diamond
+    57427, -- Earthsiege Diamond (no cd)
+    57425, -- Skyflare Diamond (no cd)
+    
     -- allCataTransmutes
     78866, -- living elements
     80244, -- Pyrium Bar
     80243, -- Truegold
 }
 
+-- vanilla
+moonclothId = 18560
+moonclothItemId = 14342
+
+saltShakerItemId = 15846
+
+arcaniteItemId = 12360 -- icon for classic transmutes
+
 -- tbc 
 primalMightId = 29688
+primalMightItemId = 23571
+
 primalMoonclothId = 26751
+primalMoonclothItemId = 21845
+
 spellclothId = 31373
+spellclothItemId = 24271
+
 shadowclothId = 36686
+shadowclothItemId = 24272
+
 brilliantGlassId = 47280
+brilliantGlassItemId = 35945
+
 voidSphereId = 28028
+voidSphereItemId = 22459
 
 -- wotlk - all on separate cooldowns
 spellweaveId = 56003
+spellweaveItemId = 41595
+
 moonshroudId = 56001
+moonshoudItemId = 41594
+
 ebonweaveId = 56002
+ebonweaveItemId = 41593
+
 minorInscriptionResearchId = 61288
-northrendInscriptionResearchId = 60893
+minorInscriptionResearchItemId = 39469 -- using moonglow ink icon
+
+northrendInscriptionResearchId = 61177
+northrendInscriptionResearchItemId = 44680 -- Assorted Writings
+
 icyPrismId = 62242
+icyPrismItemId = 44943
+
 titanSteelId = 55208
+titanStellItemId = 37663
+
 glacialBagId = 56005
-northrendAlchemyId = 61177
+glacialBagItemId = 41600
+
+northrendAlchemyId = 60893
+northrendAlchemyItemId = 7810
 
 -- cataclysm
 firePrismId = 73478
+firePrismItemId = 52304
+
 dreamOfAzsharaId = 75146
 dreamOfSkywallId = 75141
 dreamOfRagnarosId = 75145
 dreamOfDeepholmId = 75142
 dreamOfHyjalId = 75144
+dreamclothItemId = 54440
 
--- item ids
 livingElementsItemId = 30183 -- nether vortex
-dreamClothItemId = 54440
-firePrismItemId = 52304
